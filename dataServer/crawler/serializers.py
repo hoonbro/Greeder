26b8d3802598c31d11c3b4a98322f12d3cffd4e8
seungyeup <lsyes12345@gmail.com>
2021-10-17 00:24:50 +0900

    prj renew

--- dataServer/crawler/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Image
-
-class ImageSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Image
-        fields = ('id', 'keyword', 'desc', 'url') # JSON 파일에 보여질 필드를 선택
